05ffdb4d184b579efd0f0298fa253ac49fb44863
tkchu <tkchu@users.noreply.github.com>
2021-10-30 18:54:45 +0800

    父亲有时候会看你

--- Assets/FatherDoing.cs
+++ Assets/FatherDoing.cs
@@ -8,9 +8,58 @@ public class FatherDoing : MonoBehaviour
     public MeshRenderer reading;
     bool noticing = false;
 
+    float timeToNotice = 10;
+    float timeKeepNotice = 2;
+
+    public GameMaster gameMaster;
+    public void Restart()
+    {
+        noticing = false;
+        timeToNotice = Random.Range(5f, 10f);
+        timeKeepNotice = Random.Range(1f, 2f);
+    }
+    private void Start()
+    {
+        gameMaster = FindObjectOfType<GameMaster>();
+    }
     void Update()
     {
+        if (noticing)
+        {
+            if (gameMaster.isPlaying)
+            {
+                timeKeepNotice = 1f;
+            }
+            else
+            {
+                timeKeepNotice -= Time.deltaTime;
+                if(timeKeepNotice <= 0)
+                {
+                    noticing = false;
+                    timeToNotice = Random.Range(5f, 10f);
+                }
+            }
+        }
+        else
+        {
+            timeToNotice -= Time.deltaTime;
+            if (timeToNotice <= 0)
+            {
+                timeKeepNotice = Random.Range(1f, 2f);
+                noticing = true;
+            }
+        }
+
         fatherNotice.isNoticing = noticing;
-        reading.enabled = !noticing;
+        if (noticing)
+        {
+
+            reading.GetComponent<TextMesh>().text = "<color=\"red\">¶¢×ÅÄã</color>";
+        }
+        else
+        {
+            reading.GetComponent<TextMesh>().text = "¿´±¨Ö½";
+        }
+
     }
 }
