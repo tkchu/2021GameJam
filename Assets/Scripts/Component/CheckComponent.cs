19d251003239bfc703618e28c0b6bb6d8f70bc12
hewei03@corp.netease.com <hewei03@corp.netease.com>
2021-10-30 16:48:51 +0800

    添加预制件

--- Assets/Scripts/Component/CheckComponent.cs
+++ Assets/Scripts/Component/CheckComponent.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CheckComponent : MonoBehaviour
 {
+    public float anger_up_value = 10.0f;
+    public GameObject target_unit;
+    public GameObject view;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeView(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (target_unit.isPlayGame())
+        //    anger();
+    }
+
+    void anger()
+    {
+        //target_unit.health -= anger_up_value;
+    }
+
+    public void activeView(bool flag)
+    {
+        view.SetActive(flag);
     }
 }
--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -7,16 +7,58 @@ public class MovePlayer : MonoBehaviour
     public Transform born_pos;
     public Transform start_pos;
     public Transform end_pos;
-    public float one_times;
+    public float one_times = 5.0f;
+
+    private Vector3 speed;
+    private Vector3 s_p;
+    private Vector3 e_p;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = born_pos.position;
+        speed = (end_pos.position - start_pos.position) / one_times;
+        s_p = start_pos.position;
+        e_p = end_pos.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var position = transform.position + speed * Time.deltaTime;
+        var offset = e_p - s_p;
+        if (offset == Vector3.zero)
+            return;
+        var p2s = position - s_p;
+        float t;
+        if (offset.x != 0)
+            t = p2s.x / offset.x;
+        else if (offset.y != 0)
+            t = p2s.y / offset.y;
+        else if (offset.z != 0)
+            t = p2s.z / offset.z;
+        else
+            return;
+        if (t > 1 || t < 0)
+            speed *= -1.0f;
+
+        if (t > 1)
+            position = e_p - (position - e_p);
+        else if (t < 0)
+            position = s_p + (s_p - position);
+
+        transform.position = position;
+
+        Debug.Log("t: " +  t + "position: " + position);
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            var p = Random.Range(0, 10);
+            if (p > 5)
+                collision.gameObject.GetComponent<MovePlayer>().speed *= -1.0f;
+        }
     }
 }
